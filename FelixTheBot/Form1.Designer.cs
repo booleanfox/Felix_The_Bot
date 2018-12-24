cf1b7771405c01afb6a2332851804db6c2bce4d4
Takatrov Evgeny <je.mouth@gmail.com>
2018-12-24 23:52:21 +0300

    added post build events

--- FelixTheBot/Form1.Designer.cs
+++ FelixTheBot/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.buttonStart = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonStart
@@ -51,22 +52,35 @@
             this.backgroundWorker1.WorkerSupportsCancellation = true;
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Бот не работает";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(428, 129);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonStart);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Felix_The_Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button buttonStart;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- FelixTheBot/Form1.cs
+++ FelixTheBot/Form1.cs
@@ -10,19 +10,40 @@ using System.Windows.Forms;
 using System.IO;
 using AIMLbot;
 using MihaZupan;
-
+using System.Net.NetworkInformation;
 namespace FelixTheBot
 {
     public partial class Form1 : Form
     {
 
-       
-       
+        private readonly object UsersDBlock = new object();
+
         Telegram.Bot.TelegramBotClient Botik;
         string token;
         List<ProxyInfo> proxyInfos;
         HttpToSocks5Proxy proxy;
-        AIMLbot.Bot brain;
+        Bot brain;
+        Telegram.Bot.Types.User Botik_identity;
+        int offset = 0;
+
+        Dictionary<int,User> users= new Dictionary<int, User>();
+
+       
+
+        private User GetUser(int id)
+        {
+            lock (UsersDBlock)
+            {
+                if (!users.ContainsKey(id))
+                {
+                    users.Add(id, new User(id.ToString(), brain));
+
+                }
+                return users[id];
+            }
+
+        }
+
 
         public Form1()
         {
@@ -30,49 +51,115 @@ namespace FelixTheBot
             var lines = File.ReadAllLines("credentials.txt");
 
             token = lines[0];
+            proxyInfos = new List<ProxyInfo>();
             foreach (string s in lines.Skip(1))
             {
-                var t = s.Split(':');
+                 
+                var t = s.Split( new char[] { ':' },StringSplitOptions.RemoveEmptyEntries);
                 if (t.Length >= 2 && int.TryParse(t[1], out int port)) {
-                    proxyInfos.Add(new ProxyInfo(t[0], port));
+                    string addr = t[0]; 
+                    if (t.Length >= 4)
+                    {
+                        string login = t[2];
+                        string pass = t[3];
+
+                        proxyInfos.Add(new ProxyInfo(addr, port, login, pass));
+;                    }
+                    else
+                    {
+
+                        proxyInfos.Add(new ProxyInfo(addr, port));
+
+                    }
                 }
-            }
-            proxy = new HttpToSocks5Proxy(proxyInfos.ToArray());
-            proxy.ResolveHostnamesLocally = true;
-            if (!proxy.IsBypassed(new Uri("https://telegram.org/")))
-            {
-                throw new Exception("No proxies work!");
 
             }
 
-            InitBrain();
+            
+            GetBotTestProxies();
             InitBot();
+            InitBrain();
+
+          
+
+        }
+
+        void GetBotTestProxies()
+        {
+            foreach (var prox in proxyInfos)
+            {
+
+                proxy = new HttpToSocks5Proxy(new ProxyInfo[] { prox })
+                {
+                    //ResolveHostnamesLocally = true
+                };
+                
+                try
+                {
+                    Botik = new Telegram.Bot.TelegramBotClient(token, proxy);
+                    Botik_identity = Botik.GetMeAsync().Result;
+                    if (Botik_identity != null)
+                        return;
+                }
+                catch (Exception)
+                {
+
+                    
+                }
+                   
+            }
+            
+            throw new Exception("No proxies work");
+
+
         }
 
 
         void InitBrain()
         {
             brain = new Bot();
-            brain.loadSettings(Path.Combine(Environment.CurrentDirectory, Path.Combine("config", "Settings.xml")));
+            string settingsPath = Path.Combine(Environment.CurrentDirectory, Path.Combine("config", "Settings.xml"));
+            brain.loadSettings(settingsPath);
+            brain.isAcceptingUserInput = false;
             brain.loadAIMLFromFiles();
             brain.isAcceptingUserInput = true;
-            
+       
+          
         }
 
-        async void InitBot()
+         void InitBot()
         {
-            Botik = new Telegram.Bot.TelegramBotClient(token, proxy);
-            if(  ! await Botik.TestApiAsync())
-            {
-                throw new Exception("Can't reach Telegram");
-            }
-            Botik.OnMessage += Botik_OnMessage;
-            Botik.StartReceiving();
+           
+            
+           
+
+           
+            
+            
+            
+
+        }
+
+        private void Botik_OnUpdate(object sender, Telegram.Bot.Args.UpdateEventArgs e)
+        {
+            
         }
 
         private void Botik_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            
+            var message = e.Message;
+            if(message.Type == Telegram.Bot.Types.Enums.MessageType.Text && message.From.Id != Botik_identity.Id)
+            {
+                User user = GetUser(message.From.Id);
+                string text = message.Text;
+                var res = brain.Chat(text, user.UserID);
+                string reply = res.RawOutput.Trim();
+                if (reply == "")
+                    return;
+                Botik.SendTextMessageAsync(message.Chat.Id, reply);
+                
+            }
+
         }
 
         async void bw_DoWork(object sender, DoWorkEventArgs e)
@@ -81,14 +168,16 @@ namespace FelixTheBot
             try
             {
                 string settingsPath;
-                Bot botik_aiml = new Bot();
-               // botik_aiml.loadSettings(settingsPath);
-               // User myUser = new User();
-                botik_aiml.isAcceptingUserInput = false;
+                Bot botik_aiml = new Bot
+                {
+                    // botik_aiml.loadSettings(settingsPath);
+                    // User myUser = new User();
+                    isAcceptingUserInput = false
+                };
                 botik_aiml.loadAIMLFromFiles();
                 botik_aiml.isAcceptingUserInput = true;
 
-                int offset = 0; // message indent
+                // message indent
                 while (true)
                 {
                     var updates = await Botik.GetUpdatesAsync(offset);
@@ -123,12 +212,32 @@ namespace FelixTheBot
 
         void buttonStart_Click(object sender, EventArgs e)
         {
+            if (Botik.IsReceiving)
+            {
+                Botik.StopReceiving();
+                Botik.OnMessage -= Botik_OnMessage;
+                label1.ForeColor = Color.Red;
+                label1.Text = "Бот отключен";
+            }
+            else
+            {
+                Botik.StartReceiving();
+                Botik.OnMessage += Botik_OnMessage;
+                label1.ForeColor = Color.Green;
+                label1.Text = "Бот включен";
+            }
+
 
         }
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
